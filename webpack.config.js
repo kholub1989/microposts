315068207fbd7dac0406c434b83873f4ce139c80
Krisztian Holub <kholub1989@gmail.com>
2019-10-11 14:58:14 -0400

    setup enviroment, create UI

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: [
+      '@babel/polyfill',
+      './src/app.js',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development'
+}
