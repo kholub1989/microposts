ad1d192246e2113586c3d9213cb8e156e8a6e427
Krisztian Holub <kholub1989@gmail.com>
2019-10-11 19:03:14 -0400

    remove Post

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post 
 document.querySelector('.post-submit').addEventListener('click', addPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 // GET Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -33,3 +36,22 @@ function addPost() {
   })
   .catch(err => console.log(err))
 }
+
+ // Delete Post
+ function deletePost(e) {
+    
+  e.preventDefault();
+
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you shure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+}
+
